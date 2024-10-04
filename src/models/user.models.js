4dceb0f225505573a382210aec334f8ff5436d88
ritikaaggarwall <ritikaaggarwal1222@gmail.com>
2024-10-04 21:05:55 +0530

    add register controller

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -50,7 +50,7 @@ userSchema.pre("save",async function(next) {
     if(!this.isModified("password")) return next();
 
     //if modiefied
-    this.password = bcrypt.hash(this.password, 10);
+    this.password = await bcrypt.hash(this.password, 10);
     next()
 }) 
 
