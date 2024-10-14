5760009a00963f88ba825588757d654d70a6e0f0
ritikaaggarwall <ritikaaggarwal1222@gmail.com>
2024-10-14 20:16:59 +0530

    setting bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
-import { emit } from "nodemon";
+
 import { asyncHandler } from "../utils/asyncHandler.js";
-import {ApiError} from "..utils/ApiError.js";
+import {ApiError} from "../utils/ApiError.js";
 import {User} from "../models/user.models.js";
 import {uploadOnCloudinary} from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -32,7 +32,7 @@ const registerUser = asyncHandler( async (req,res) => {
         throw new ApiError(400, "all fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{username},{email}]
     })
 
